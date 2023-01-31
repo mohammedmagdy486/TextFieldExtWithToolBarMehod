1d4c2b452dab7cfc4e771adf55bb7b3913ec5f08
mohammed Magdy <mohammedmagdy486@gmail.com>
2023-01-31 14:48:52 +0200

    Add files via upload

---
+++ UITextField+Ext2.swift
@@ -0,0 +1,23 @@
+//
+//  UITextField+Ext2.swift
+//  MIT CV
+//
+//  Created by AMN on 1/31/23.
+//  Copyright © 2023 Nura Abd Al Majeed. All rights reserved.
+//
+
+import Foundation
+extension UITextField {
+    func setupToolbar(){
+        let toolBar = UIToolbar()
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = .black
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done".localized(), style: .plain, target: self, action: #selector(self.doneTapped))
+        doneButton.tintColor = .systemBlue
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        self.inputAccessoryView = toolBar
+    }
+}
